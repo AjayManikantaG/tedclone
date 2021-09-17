7a6c9e5eec2a56965881e1ee79771262a5becb59
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-09-17 21:52:58 +0530

    Add navbar changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,11 +4,13 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/TED_wordmark.svg/2560px-TED_wordmark.svg.png'
-        alt='Ted: Ideas worth spreading'
-      />
-      <ul>
+      <div className='logo'>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/TED_wordmark.svg/2560px-TED_wordmark.svg.png'
+          alt='Ted: Ideas worth spreading'
+        />
+      </div>
+      <ul className='navbar-list'>
         <li>
           <a href='#' alt='Watch'>
             Watch
@@ -35,8 +37,14 @@ const Navbar = () => {
           </a>
         </li>
       </ul>
-      <button>Membership</button>
-      <button>Sign in</button>
+      <div className='action-btn'>
+        <div className='action-btn-member'>
+          <button>Membership</button>
+        </div>
+        <div className='action-btn-sign-in'>
+          <button>Sign in</button>
+        </div>
+      </div>
     </div>
   );
 };
